99a1320283e116f51b61a291787b2c2f11c52039
Dwight Millar <millar.dwight@gmail.com>
2019-11-19 12:04:23 -0800

    created jQuery http requests, appended profile data to DOM

---
+++ main.js
@@ -0,0 +1,58 @@
+var nameArray = [];
+var descriptionArray = [];
+var imageArray = [];
+var employees = [];
+var imagesFolder = '';
+
+
+window.onload = getNames;
+
+
+function getNames() {
+  $.get("https://techi.envivent.com/names.json", (data, status) => {
+    nameArray = data.employees;
+    if (status !== 'success') alert('Failed to fetch names');
+    getDescriptions();
+  })
+}
+
+function getDescriptions() {
+  $.get("https://techi.envivent.com/description.json", (data, status) => {
+    descriptionArray = data.employees;
+    if (status !== 'success') alert('Failed to fetch descriptions');
+    getImages();
+  })
+}
+
+function getImages() {
+  $.get("https://techi.envivent.com/images.json", (data, status) => {
+    imageArray = data.employees;
+    imagesFolder = data["images-folder"];
+    if (status !== 'success') alert('Failed to fetch images');
+    mergeData();
+  })
+}
+
+function mergeData() {
+  for (let employeeIndex in nameArray) {
+    let descriptionArrayIndex = descriptionArray.findIndex( (element) => element.id === nameArray[employeeIndex].id );
+    let imageArrayIndex = imageArray.findIndex( (element) => element.id === nameArray[employeeIndex].id );
+
+    employees.push({...nameArray[employeeIndex], ...descriptionArray[descriptionArrayIndex], ...imageArray[imageArrayIndex]});
+  }
+  createDOMElements();
+}
+
+function createDOMElements() {
+  console.log(employees);
+
+  for(let profileIndex = 0; profileIndex < 3; profileIndex++) {
+    let employeeIndex = Math.floor((Math.random() * employees.length));
+    console.log(employees);
+    $(".container").append(`<img src='${imagesFolder}${employees[employeeIndex].full}' alt='employee'>`)
+    $(".container").append(`<h1>${employees[employeeIndex].first_name} ${employees[employeeIndex].last_name}</h1>`)
+    $(".container").append(`<h3>${employees[employeeIndex].title}</h3>`)
+    $(".container").append(`<p>${employees[employeeIndex].description}</p>`)
+    employees.splice(employeeIndex, 1);
+  }
+}
